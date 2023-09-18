055c2c49667cf7f0c9df9b940f8908a805850c77
ojo gbotemi <hommzmum@gmail.com>
2023-09-18 20:03:40 +0100

    jwt token added

--- routes/masterRoute.js
+++ routes/masterRoute.js
@@ -4,6 +4,9 @@ const express = require("express");
 const router = express.Router();
 const passport = require("passport");
 require('../config/passport');
+router.get("/", (req, res) => {
+    res.json("this is home")
+});
 router.post("/instagram-clone/signup",userController.sign_up);
 router.post("/instagram-clone/signin",userController.sign_in);
 router.get("/instagram-clone",passport.authenticate('jwt', { session: false }),feedpostController.feedpost);
